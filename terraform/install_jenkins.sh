ade5ae2a8190b9eab2433a8716941a75b465a523
Vitale Mazo <vitalemazo@gmail.com>
2020-08-24 11:36:21 -0500

    Update install_jenkins.sh
    Updated --nogpgcheck for Jenkins yum install

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -23,7 +23,7 @@ sudo chkconfig docker on
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
+yum install -y jenkins --nogpgcheck
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
