06fd8cc47e6cf7c03725aebc2e525fd154d0e405
Ghost <vitalemazo@gmail.com>
2020-08-25 11:30:59 -0500

    updated no --nogpgcheck on yum install

--- terraform/install_kubectl_hal.sh
+++ terraform/install_kubectl_hal.sh
@@ -5,8 +5,8 @@ sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
+gpgcheck=0
+repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 yum install -y kubectl
