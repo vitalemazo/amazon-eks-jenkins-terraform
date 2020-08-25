4361426dcfbc17281ca4f51f69497f662f916405
Ghost <vitalemazo@gmail.com>
2020-08-25 12:06:31 -0500

    updated resource iam role codedeploy

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,9 +1,37 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "jenkins-bucket" {
+  bucket = "my-tf-jenkins-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "Jenkins bucket"
     Environment = "Dev"
+  }
+}
+
+
+
+
+
+resource "aws_iam_role" "CodeDeploy" {
+  name = "CodeDeploy"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    tag-key = "tag-CodeDeploy"
   }
 }
